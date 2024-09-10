e2c10ffd781f58551cc90f8ea19da96c57fbb548
Kyle <rocktavious@icloud.com>
2024-09-10 09:09:24 -0500

    Update kubectl.rb

--- Formula/kubectl.rb
+++ Formula/kubectl.rb
@@ -12,20 +12,18 @@ class Kubectl < Formula
   depends_on "jq"
   depends_on :macos
 
-  on_arm do
-    url "https://github.com/opslevel/kubectl-opslevel/archive/refs/tags/v2024.9.5.tar.gz"
-    sha256 "fcd5b5171880b644657e67d50be95face455712c2f051910a11c08ad929174dd"
+  url "https://github.com/opslevel/kubectl-opslevel/archive/refs/tags/v2024.9.5.tar.gz"
+  sha256 "1ec42905a994e5a52c451af54192cc2e9808fb1ef635a83b73a8b1f7a463f650"
 
-    def install
-      ENV['CGO_ENABLED'] = '1'
-      ENV['CGO_CFLAGS'] = "-I#{Formula['jq'].opt_include}"
-      ENV['CGO_LDFLAGS'] = "-L#{Formula['jq'].opt_lib}"
-      cd 'src' do
-        system 'go', 'build', *std_go_args(
-          output: bin / 'kubectl-opslevel',
-          ldflags: "-s -w -X main.version=2024.9.5 -X main.commit=a7ddc37"
-        )
-      end
+  def install
+    ENV['CGO_ENABLED'] = '1'
+    ENV['CGO_CFLAGS'] = "-I#{Formula['jq'].opt_include}"
+    ENV['CGO_LDFLAGS'] = "-L#{Formula['jq'].opt_lib}"
+    cd 'src' do
+      system 'go', 'build', *std_go_args(
+        output: bin / 'kubectl-opslevel',
+        ldflags: "-s -w -X main.version=2024.9.5 -X main.commit=a7ddc37"
+      )
     end
   end
 
