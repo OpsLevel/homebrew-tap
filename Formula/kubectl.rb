c60b709ef101d295c4ab3fda4ae0f633411ae6cd
David Bloss <david@opslevel.com>
2024-09-27 08:47:55 -0500

    correct latest sha for arm architecture

--- Formula/kubectl.rb
+++ Formula/kubectl.rb
@@ -12,8 +12,12 @@ class Kubectl < Formula
   depends_on "jq"
   depends_on :macos
 
-  url "https://github.com/opslevel/kubectl-opslevel/archive/refs/tags/v2024.9.5.tar.gz"
-  sha256 "1ec42905a994e5a52c451af54192cc2e9808fb1ef635a83b73a8b1f7a463f650"
+  url 'https://github.com/opslevel/kubectl-opslevel/archive/refs/tags/v2024.9.5.tar.gz'
+  sha256 '1ec42905a994e5a52c451af54192cc2e9808fb1ef635a83b73a8b1f7a463f650'
+
+  on_arm do
+    sha256 'fcd5b5171880b644657e67d50be95face455712c2f051910a11c08ad929174dd'
+  end
 
   def install
     ENV['CGO_ENABLED'] = '1'
